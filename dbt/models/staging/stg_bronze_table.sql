eb0e51e7cae867aaa2aa9e7193bc56a7cf0b512e
Anqi Li <zhuchangchang89@gmail.com>
2025-04-14 19:51:22 +0000

    create staging table and make it incremental

---
+++ dbt/models/staging/stg_bronze_table.sql
@@ -0,0 +1,67 @@
+{{ config(
+    materialized='incremental',
+    unique_key='generated_primary_key'
+)
+
+}}
+
+with source as (
+
+    select * from {{ source('staging', 'bronze_table') }}
+
+),
+
+renamed as (
+
+    select
+        {{ dbt_utils.generate_surrogate_key(['valid', 'station']) }} as observation_id,
+        valid as observation_time,
+        lon,
+        lat,
+        tmpf as air_temperature,
+        dwpf as dew_point_temperature,
+        relh as relative_humidity,
+        drct as wind_direction,
+        sknt as wind_speed,
+        p01i as precipitation,
+        alti as pressure_altimeter,
+        mslp as sea_level_pressure,
+        vsby as visibility,
+        gust as wind_gust,
+        skyc1 as sky_level_1_coverage,
+        skyc2 as sky_level_2_coverage,
+        skyc3 as sky_level_3_coverage,
+        skyc4 as sky_level_4_coverage,
+        skyl1 as sky_level_1_altitude,
+        skyl2 as sky_level_2_altitude,
+        skyl3 as sky_level_3_altitude,
+        skyl4 as sky_level_4_altitude,
+        wxcodes as present_weather_code,
+        ice_accretion_1hr,
+        ice_accretion_3hr,
+        ice_accretion_6hr,
+        peak_wind_gust,
+        peak_wind_drct,
+        peak_wind_time,
+        feel as apparent_temperature,
+        snowdepth as snow_depth,
+        station
+
+    from source
+
+)
+
+select * from renamed
+
+-- use {{ this }} to refer to the target table of the model
+-- compare the max timestamp of the existing target table and source table
+-- only load the new data
+{% if is_incremental() %}
+
+where valid > (
+    -- use coalesce for first time load bronze_table
+    select coalesce(max(valid), '1900-01-01')
+    from {{ this }}
+)
+
+{% endif %}
