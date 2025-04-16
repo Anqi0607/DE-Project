a949b60ef16148d104be2404f1dcc02b3a59a052
Anqi Li <zhuchangchang89@gmail.com>
2025-04-16 20:48:37 +0000

    add tests for agg tables

--- dbt/macros/compile_models_for_yml_files.sql
+++ dbt/macros/compile_models_for_yml_files.sql
@@ -4,3 +4,20 @@
 )}}
 
 
+{% set models_to_generate = [
+    'customers',
+    'orders'
+] %}
+
+{{ codegen.generate_model_yaml(
+    model_names = models_to_generate
+) }}
+
+{% set models_to_generate = codegen.get_models(
+    directory='core',
+    prefix='agg_'
+) %}
+
+{{ codegen.generate_model_yaml(
+    model_names = models_to_generate
+) }}
