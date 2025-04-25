cfc4a329fef9f7e9be8aa9156194c5fd35a535d7
anqi0607 <anqili0607@gmail.com>
2025-04-25 16:00:21 -0400

    add integration test to ci workflow

--- tests/integration/test_data_transformation_dag.py
+++ tests/integration/test_data_transformation_dag.py
@@ -12,7 +12,8 @@ import pytest
 from datetime import datetime, timedelta
 from airflow.models import DagBag, TaskInstance
 from airflow.utils.state import State
-from google.cloud import storage
+from google.cloud import storage, bigquery
+from pyspark.sql import SparkSession
 
 import config
 
@@ -43,6 +44,8 @@ def test_dag_end_to_end():
     client = storage.Client()
     bucket = client.bucket(config.BUCKET_NAME)
 
+    spark = SparkSession.builder.appName("Test Parquet in GCS").getOrCreate()
+
     for station in stations:
         raw_prefix = f"{config.GCS_PREFIX}/2023/01/station={station}/"
         bronze_prefix = raw_prefix.replace("Raw", "Bronze")
@@ -52,5 +55,88 @@ def test_dag_end_to_end():
         parts = [b.name for b in blobs]
         assert any(f.endswith(".parquet") for f in parts), f"Expected parquet part files not saved in gcs, but got {parts}"
     
+        # assert data是否被正确transform成
+        parquet_path = f"gs://{config.BUCKET_NAME}/{bronze_prefix}"
+        df = spark.read.parquet(parquet_path)
+        # no duplicates
+        assert df.count() == df.distinct().count(), "duplicates found"
+        # data type
+        assert dict(df.dtypes)["valid"] == "timestamp", "valid is not timestamp"
+        # numeric columns
+        numeric_columns = [
+            "lon", "lat", "tmpf", "dwpf", "relh", "drct", "sknt", "p01i",
+            "alti", "mslp", "vsby", "gust", "skyl1", "skyl2", "skyl3", "skyl4",
+            "ice_accretion_1hr", "ice_accretion_3hr", "ice_accretion_6hr",
+            "peak_wind_gust", "peak_wind_drct", "peak_wind_time", "feel", "snowdepth"
+        ]
+        for col in numeric_columns:
+            col_type = dict(df.dtypes).get(col)
+            assert col_type == "double", f"Expected {col} column to be of type 'double', but got {col_type}"
+        
+    # ---- transform_raw_data ----
+    ti_check = TaskInstance(task=dag.get_task("check_bronze_data"), execution_date=exec_date)
+    ti_check.run(ignore_ti_state=True)
+    assert ti_check.state == State.SUCCESS
 
+    # ---- load_bronze_table ----
+    ti_load = TaskInstance(task=dag.get_task("load_bronze_table"), execution_date=exec_date)
+    ti_load.run(ignore_ti_state=True)
+    assert ti_load.state == State.SUCCESS
 
+    # assert table exists
+    bq_client = bigquery.Client()
+    dataset_id = f"{config.GCP_PROJECT_ID}.{config.GCP_BIGQUERY_DATASET}"
+    table_id = f"{dataset_id}.bronze_table"
+    try:
+        table = bq_client.get_table(table_id)  # Try fetching the table
+        assert table is not None, f"BigQuery table {table_id} does not exist."
+    except bigquery.exceptions.NotFound:
+        pytest.fail(f"BigQuery table {table_id} does not exist")
+    
+    # assert table schema 
+    table = bq_client.get_table(table_id)
+    schema = table.schema
+    expected_columns = [
+    ("station", "STRING"),
+    ("valid", "TIMESTAMP"),
+    ("lon", "FLOAT"),
+    ("lat", "FLOAT"),
+    ("tmpf", "FLOAT"),
+    ("dwpf", "FLOAT"),
+    ("relh", "FLOAT"),
+    ("drct", "FLOAT"),
+    ("sknt", "FLOAT"),
+    ("p01i", "FLOAT"),
+    ("alti", "FLOAT"),
+    ("mslp", "FLOAT"),
+    ("vsby", "FLOAT"),
+    ("gust", "FLOAT"),
+    ("skyc1", "STRING"),
+    ("skyc2", "STRING"),
+    ("skyc3", "STRING"),
+    ("skyc4", "STRING"),
+    ("skyl1", "FLOAT"),
+    ("skyl2", "FLOAT"),
+    ("skyl3", "FLOAT"),
+    ("skyl4", "FLOAT"),
+    ("wxcodes", "STRING"),
+    ("ice_accretion_1hr", "FLOAT"),
+    ("ice_accretion_3hr", "FLOAT"),
+    ("ice_accretion_6hr", "FLOAT"),
+    ("peak_wind_gust", "FLOAT"),
+    ("peak_wind_drct", "FLOAT"),
+    ("peak_wind_time", "FLOAT"),
+    ("feel", "FLOAT"),
+    ("snowdepth", "FLOAT")
+]
+    
+    for col, dtype in expected_columns:
+        assert any(field.name == col and field.field_type == dtype for field in schema), \
+            f"Column {col} with expected data type {dtype} not found in BigQuery table schema"
+    
+    # assert data is loaded to table 
+    query = f"SELECT COUNT(*) FROM `{table_id}`"
+    result = bq_client.query(query).result()
+    row_count = next(result).values()[0]
+    
+    assert row_count > 0, f"Table {table_id} has no data loaded."
