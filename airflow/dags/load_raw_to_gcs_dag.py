680ea5c3e774f7ce7eb7890357accc4e3dabb059
anqi0607 <anqili0607@gmail.com>
2025-04-30 17:37:16 -0400

    creat bronze table in big query on Dataproc

--- airflow/dags/load_raw_to_gcs_dag.py
+++ airflow/dags/load_raw_to_gcs_dag.py
@@ -54,15 +54,37 @@ def build_bronze_transform_job(**kwargs):
         },
     }
 
+@task
+def build_create_bq_table_job(**kwargs):
+    execution_date = kwargs["data_interval_start"]
+    ym = execution_date.strftime("%Y%m")
+
+    parquet_prefix = config.GCS_PARQUET_PREFIX
+    bronze_prefix = parquet_prefix.replace("PARQUET", "Bronze-Dataproc")
+    input_path = f"gs://{config.BUCKET_NAME}/{bronze_prefix}/{ym}/"
+    return {
+        "reference": {"project_id": config.GCP_PROJECT_ID},
+        "placement": {"cluster_name": f"metar-cluster-{execution_date.strftime('%Y%m%d')}"},
+        "pyspark_job": {
+            "main_python_file_uri": f"gs://{config.BUCKET_NAME}/METAR/spark-jobs/create_bq_table_gcp.py",
+            "args": [input_path, config.GCP_PROJECT_ID, config.GCP_BIGQUERY_DATASET, config.TEMP_BUCKET],
+        },
+    }
 
 cluster_config = {
     "master_config": {
         "num_instances": 1,
         "machine_type_uri": "n1-standard-2",
+        "disk_config": {
+            "boot_disk_size_gb": 50
+        },
     },
     "worker_config": {
         "num_instances": 2,
         "machine_type_uri": "n1-standard-2",
+        "disk_config": {
+            "boot_disk_size_gb": 100
+        },
     },
     "gce_cluster_config": {
         "zone_uri": "us-central1-f",
@@ -125,15 +147,32 @@ with DAG(
             project_id=config.GCP_PROJECT_ID,
         )
 
-    t7_delete_cluster = DataprocDeleteClusterOperator(
-        task_id="delete_cluster",
-        project_id=config.GCP_PROJECT_ID,
-        cluster_name=CLUSTER_NAME_TEMPLATE,
+    t7_table_job_config = build_create_bq_table_job()
+
+    t8_submit_table_spark = DataprocSubmitJobOperator(
+        task_id="submit_table_spark_job",
+        job=t7_table_job_config,
         region=config.GCP_REGION,
-        trigger_rule="all_done",  # 即使任务失败也删除
+        project_id= config.GCP_PROJECT_ID,
     )
 
+    # t9_delete_cluster = DataprocDeleteClusterOperator(
+    #     task_id="delete_cluster",
+    #     project_id=config.GCP_PROJECT_ID,
+    #     cluster_name=CLUSTER_NAME_TEMPLATE,
+    #     region=config.GCP_REGION,
+    #     trigger_rule="all_done",  # 即使任务失败也删除
+    # )
 
-    t1_download_csv_to_gcs >> t2_create_cluster >> t3_spark_job_config >> t4_submit_convert_spark >> t5_transform_job_config >> t6_submit_transform_spark >> t7_delete_cluster
-
+    (
+    t1_download_csv_to_gcs 
+    >> t2_create_cluster 
+    >> t3_spark_job_config 
+    >> t4_submit_convert_spark
+    >> t5_transform_job_config 
+    >> t6_submit_transform_spark
+    >> t7_table_job_config 
+    >> t8_submit_table_spark 
+    # >> t9_delete_cluster
+)
 
--- airflow/scripts/transform_raw_data_gcp.py
+++ airflow/scripts/transform_raw_data_gcp.py
@@ -1,6 +1,5 @@
 import sys
-import os
-import glob
+
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType
 from pyspark.sql import functions as F
